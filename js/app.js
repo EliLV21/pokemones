b83c0245d5594e109d16293b4a4d0b69fe17ec6e
Miriam Elizabeth Laguna Velazquez <Miriam Elizabeth Laguna Velazquez>
2017-06-12 17:05:06 -0500

    2a parte a medias

--- js/app.js
+++ js/app.js
@@ -1,25 +1,98 @@
+var pokemones = [
+  {
+      "src": "assets/img/bulbasaur.png"
+  },
+  {
+      "src": "assets/img/ivysaur.png"
+  },
+  {
+      "src": "assets/img/venusaur.png"
+  },
+  {
+      "src": "assets/img/charmander.png"
+  },
+  {
+      "src": "assets/img/charmeleon.png"
+  },
+  {
+      "src": "assets/img/charizard.png"
+  },
+  {
+      "src": "assets/img/squirtle.png"
+  },
+  {
+      "src": "assets/img/wartortle.png"
+  },
+  {
+      "src": "assets/img/blastoise.png"
+  },
+  {
+      "src": "assets/img/caterpie.png"
+  },
+  {
+      "src": "assets/img/metapod.png"
+  },
+  {
+      "src": "assets/img/butterfree.png"
+  },
+  {
+      "src": "assets/img/weedle.png"
+  },
+  {
+      "src": "assets/img/kakuna.png"
+  },
+  {
+      "src": "assets/img/beedrill.png"
+  },
+  {
+      "src": "assets/img/pidgey.png"
+  },
+  {
+      "src": "assets/img/pidgeotto.png"
+  },
+  {
+      "src": "assets/img/pidgeot.png"
+  },
+  {
+      "src": "assets/img/rattata.png"
+  },
+  {
+      "src": "assets/img/raticate.png"
+  },
+];
+
 function cargarPagina(){
-  var plantillaPokemons = '<section class="row">' +
-    '<section class="col s4">' +
-      '<img data-pokemon="__data__" src="__imagen__" alt="">'  +
-      '<h4>__nombre__</h4>' +
-    '</section>' +
+  var plantillaPokemons = '<section class="col s3 contenidoPokemones">' +
+    '<a href="#modal1"><img data-pokemon="__data__" class="imagenesPokemones" src="__imagen__" alt=""></a>'  +
+    '<span>__nombre__</span>' +
   '</section>';
 
   $.getJSON("http://pokeapi.co/api/v2/pokemon/", function(response){
-    console.log(response);
     var pokemons = response.results;
-    var $contenedor = $("#contenedorPokemon");
-    nombrePokemones(pokemons);
+    nombrePokemones(pokemons, pokemones);
   });
-  var nombrePokemones = function(pokemons){
+  var nombrePokemones = function(pokemons, pokemones){
+    var $contenedor = $("#contenedorPokemon");
+    var plantillaFinal = "";
     pokemons.forEach(function(pokemon, indice){
-      plantillaPokemons.replace("__nombre__", pokemon.name)
-      $contenedor.html(plantillaPokemons);
+      plantillaFinal += plantillaPokemons.replace("__nombre__", pokemon.name)
+                          .replace("__imagen__", pokemones[indice].src)
+
     });
+      $contenedor.html(plantillaFinal);
   };
 
-  $(".button-collapse").sideNav();
+  function mostrarModal(){
+      $.getJSON("http://pokeapi.co/api/v2/pokemon-species/", function(response){
+        var colorPokemon = response.result.color;
+
+
+      });
+
+  };
+  console.log(mostrarModal);
+
+$('#modal1').modal();
 
 };
 
